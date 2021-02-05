52effac0add70d74cae22a12ae7cb37de58b15c7
Daniel Lahyani <dlahyani@lightricks.com>
2021-02-05 16:19:15 +0200

    AoC 2020 Day 04 solution.

---
+++ 2020/04/solution.py
@@ -0,0 +1,74 @@
+from re import match
+from typing import Dict
+
+
+def verify_birth_year(birth_year: str) -> bool:
+    return match("^[0-9]{4}$", birth_year) and 1920 <= int(birth_year) <= 2002
+
+def verify_issue_year(issue_year: str) -> bool:
+    return match("^[0-9]{4}$", issue_year) and 2010 <= int(issue_year) <= 2020
+
+def verify_expiration_year(expiration_year: str) -> bool:
+    return match("^[0-9]{4}$", expiration_year) and 2020 <= int(expiration_year) <= 2030
+
+def verify_height(height: str) -> bool:
+    try:
+        h, u = match("^([0-9]+)(cm|in)$", height).groups()
+        return (u == "cm" and 150 <= int(h) <= 193) or (u == "in" and 59 <= int(h) <= 76)
+    except AttributeError:
+        return False
+
+def verify_hair_color(hair_color: str) -> bool:
+    return match("^#[0-9a-f]{6}$", hair_color) is not None
+
+def verify_eye_color(eye_color: str) -> bool:
+    return eye_color in {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"}
+
+def verify_passport_id(passport_id: str) -> bool:
+    return match("^[0-9]{9}$", passport_id) is not None
+
+
+PASSPORT_REQUIRED_FIELDS = {
+    "byr": verify_birth_year,
+    "iyr": verify_issue_year,
+    "eyr": verify_expiration_year,
+    "hgt": verify_height,
+    "hcl": verify_hair_color,
+    "ecl": verify_eye_color,
+    "pid": verify_passport_id
+}
+
+def create_passport(passport_raw: str) -> Dict[str, str]:
+    """
+    Convert raw textual passport to a dictionary of fields without validating the passport.
+    """
+    return {f[0]: f[1] for f in map(lambda p: p.split(":"), passport_raw.split())}
+
+def verify_passport(passport: Dict[str, str]) -> bool:
+    """
+    Verify a passport in dictionary format.
+    Returns True if the passport contains all required fields.
+    """
+    return all([f in passport for f in PASSPORT_REQUIRED_FIELDS])
+
+def verify_passport_and_fields(passport: Dict[str, str]) -> bool:
+    """
+    Verify a passport in dictionary format.
+    Returns True if the passport contains all required fields and all of them contain a valid value.
+    """
+    try:
+        return all([f in passport and v(passport[f]) for f, v in PASSPORT_REQUIRED_FIELDS.items()])
+    except ValueError:
+        return False
+
+
+if __name__ == "__main__":
+    passports = [create_passport(p) for p in open("2020/04/input.txt", "r").read().split("\n\n")]
+
+    # Part 1
+    valid_passports = filter(verify_passport, passports)
+    print(f"Valid passports: {len(list(valid_passports))}")
+
+    # Part 2
+    valid_passports = filter(verify_passport_and_fields, passports)
+    print(f"Valid passports: {len(list(valid_passports))}")
---
+++ 2020/04/test_solution.py
@@ -0,0 +1,228 @@
+from unittest import TestCase
+
+import solution
+
+class VerifyBirthYearTests(TestCase):
+    def test_not_a_number(self):
+        with self.assertRaises(ValueError):
+            solution.verify_birth_year("not-a-number")
+
+    def test_not_a_number_suffix(self):
+        with self.assertRaises(ValueError):
+            solution.verify_birth_year("1990g")
+    
+    def test_not_a_number_prefix(self):
+        with self.assertRaises(ValueError):
+            solution.verify_birth_year("x1990")
+    
+    def test_too_many_digits(self):
+        with self.assertRaises(ValueError):
+            solution.verify_birth_year("01990")
+
+    def test_too_low(self):
+        with self.assertRaises(ValueError):
+            solution.verify_birth_year("1919")
+    
+    def test_too_high(self):
+        with self.assertRaises(ValueError):
+            solution.verify_birth_year("2003")
+
+    def test_min(self):
+        self.assertEqual(
+            solution.verify_birth_year("1920"),
+            1920
+        )
+    
+    def test_max(self):
+        self.assertEqual(
+            solution.verify_birth_year("2002"),
+            2002
+        )
+
+    def test_mid_range(self):
+        self.assertEqual(
+            solution.verify_birth_year("1990"),
+            1990
+        )
+
+
+class VerifyIssueYearTests(TestCase):
+    def test_not_a_number(self):
+        with self.assertRaises(ValueError):
+            solution.verify_issue_year("not-a-number")
+
+    def test_not_a_number_2(self):
+        with self.assertRaises(ValueError):
+            solution.verify_issue_year("2015g")
+    
+    def test_not_a_number_3(self):
+        with self.assertRaises(ValueError):
+            solution.verify_issue_year("x2015")
+
+    def test_too_many_digits(self):
+        with self.assertRaises(ValueError):
+            solution.verify_birth_year("02015")
+
+    def test_too_low(self):
+        with self.assertRaises(ValueError):
+            solution.verify_issue_year("2009")
+    
+    def test_too_high(self):
+        with self.assertRaises(ValueError):
+            solution.verify_issue_year("2021")
+
+    def test_min(self):
+        self.assertEqual(
+            solution.verify_issue_year("2010"),
+            2010
+        )
+    
+    def test_max(self):
+        self.assertEqual(
+            solution.verify_issue_year("2020"),
+            2020
+        )
+
+    def test_mid_range(self):
+        self.assertEqual(
+            solution.verify_issue_year("2015"),
+            2015
+        )
+
+
+class VerifyExpirationYearTests(TestCase):
+    def test_not_a_number(self):
+        with self.assertRaises(ValueError):
+            solution.verify_expiration_year("not-a-number")
+
+    def test_not_a_number_2(self):
+        with self.assertRaises(ValueError):
+            solution.verify_expiration_year("2025g")
+    
+    def test_not_a_number_3(self):
+        with self.assertRaises(ValueError):
+            solution.verify_expiration_year("x2025")
+
+    def test_too_many_digits(self):
+        with self.assertRaises(ValueError):
+            solution.verify_birth_year("02025")
+
+    def test_too_low(self):
+        with self.assertRaises(ValueError):
+            solution.verify_expiration_year("2019")
+    
+    def test_too_high(self):
+        with self.assertRaises(ValueError):
+            solution.verify_expiration_year("2031")
+
+    def test_min(self):
+        self.assertEqual(
+            solution.verify_expiration_year("2020"),
+            2020
+        )
+    
+    def test_max(self):
+        self.assertEqual(
+            solution.verify_expiration_year("2030"),
+            2030
+        )
+
+    def test_mid_range(self):
+        self.assertEqual(
+            solution.verify_expiration_year("2025"),
+            2025
+        )
+
+
+class VerifyHeightTests(TestCase):
+    def test_invalid_format(self):
+        with self.assertRaises(ValueError):
+            solution.verify_height("what's-that")
+
+    def test_invalid_format_number(self):
+        with self.assertRaises(ValueError):
+            solution.verify_height("x160cm")
+    
+    def test_invalid_format_unit(self):
+        with self.assertRaises(ValueError):
+            solution.verify_height("160kcm")
+
+    def test_height_cm_too_low(self):
+        with self.assertRaises(ValueError):
+            solution.verify_height("149cm")
+
+    def test_height_cm_too_high(self):
+        with self.assertRaises(ValueError):
+            solution.verify_height("194cm")
+
+    def test_height_in_too_low(self):
+        with self.assertRaises(ValueError):
+            solution.verify_height("58in")
+
+    def test_height_in_too_high(self):
+        with self.assertRaises(ValueError):
+            solution.verify_height("77in")
+
+    def test_height_cm_min(self):
+        self.assertEqual(
+            solution.verify_height("150cm"),
+            (150, "cm")
+        )
+
+    def test_height_cm_max(self):
+        self.assertEqual(
+            solution.verify_height("193cm"),
+            (193, "cm")
+        )
+
+    def test_height_cm_mid(self):
+        self.assertEqual(
+            solution.verify_height("170cm"),
+            (170, "cm")
+        )
+
+    def test_height_in_min(self):
+        self.assertEqual(
+            solution.verify_height("59in"),
+            (59, "in")
+        )
+
+    def test_height_cm_max(self):
+        self.assertEqual(
+            solution.verify_height("76in"),
+            (76, "in")
+        )
+
+    def test_height_cm_mid(self):
+        self.assertEqual(
+            solution.verify_height("60in"),
+            (60, "in")
+        )
+
+
+class TestVerifyHairColor(TestCase):
+    def test_missing_sharp_prefix(self):
+        with self.assertRaises(ValueError):
+            solution.verify_hair_color("88bb33")
+
+    def test_invalid_digit(self):
+        with self.assertRaises(ValueError):
+            solution.verify_hair_color("#88gb33")
+
+    def test_not_enough_digits(self):
+        with self.assertRaises(ValueError):
+            solution.verify_hair_color("#88bb3")
+            
+    def test_not_invalid_prefix(self):
+        with self.assertRaises(ValueError):
+            solution.verify_hair_color("xxx#88bb33")
+
+    def test_not_invalid_suffix(self):
+        with self.assertRaises(ValueError):
+            solution.verify_hair_color("#88bb33xxx")
+
+    def test_valid_color(self):
+        self.assertEqual(
+            solution.verify_hair_color("#090a88"),
+            "#090a88"
+        )
