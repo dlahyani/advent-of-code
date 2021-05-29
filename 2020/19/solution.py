61628006a2a99a1f9e363a19645f07038c0be385
Daniel Lahyani <dlahyani@lightricks.com>
2021-05-29 16:10:11 +0300

    2020/19: Prepare for part 2.

--- 2020/19/solution.py
+++ 2020/19/solution.py
@@ -1,171 +1,132 @@
 from enum import Enum
-from typing import Any, Iterable, Dict, Tuple, Callable, Protocol
+from typing import Any, Iterable, Dict, Tuple
 
 import regex
 
 
-class Rule(Protocol):
-    def match(self, line: str, index: int = 0, match_all: bool = False) -> int:
-        pass
+class RuleType(Enum):
+    SimpleRule = 0
+    ChainedRule = 1,
+    FlexRule = 2,
 
 
-class SimpleRule:
-    def __init__(self, rule_str: str):
-        self.rule_str = rule_str
-
-    def match(self, line: str, index: int = 0, match_all: bool = False) -> int:
-        matched = len(self.rule_str) if line[index:].startswith(self.rule_str) else 0
-        return matched if not match_all or index + matched == len(line) else 0
-
-    def __repr__(self):
-        return f"<SimpleRule: '{str(self)}'>"
-
-    def __str__(self):
-        return self.rule_str
+class RuleDescriptor:
+    def __init__(self, rule_type: RuleType, rule_data: Any):
+        self.type = rule_type
+        self.data = rule_data
 
 
-class ChainedRule:
-    def __init__(self, sub_rules: Tuple[Rule]):
-        self.sub_rules = sub_rules
-
-    def match(self, line: str, index: int = 0, match_all: bool = False) -> int:
-        total_digested = 0
-        for rule in self.sub_rules:
-            digested = rule.match(line, index + total_digested)
-            if digested == 0:
-                return 0
-            assert index + total_digested + digested <= len(line)
-            total_digested += digested
-
-        return total_digested if not match_all or index + total_digested == len(line) else 0
-
-    def __repr__(self):
-        return f"<ChainedRule: '{str(self)}'>"
-
-    def __str__(self):
-        return '+'.join([str(sr) for sr in self.sub_rules])
+RulesMap = Dict[int, RuleDescriptor]
 
 
-class FlexRule:
-    def __init__(self, sub_rules: Tuple[Rule]):
-        self.sub_rules = sub_rules
+def match_rule(rule_id: int, line: str, rules_map: RulesMap, index: int = 0, match_all: bool = True) -> bool:
+    matched = _match_rule(rules_map[rule_id], line, rules_map, index)
+    if matched == 0:
+        return False
 
-    def match(self, line: str, index: int = 0, match_all: bool = False) -> int:
-        for rule in self.sub_rules:
-            digested = rule.match(line, index, match_all)
-            assert index + digested <= len(line)
-            if digested != 0:
-                return digested
+    print(f"'{line[:index]}[{line[index:index+matched]}]{line[index+matched:]}' {matched} chars matched rule #{rule_id}")
+    return matched if not match_all or matched + index == len(line) else 0
 
-        return 0
 
-    def __repr__(self):
-        return f"<FlexRule: '{str(self)}'>"
+def _match_rule(rule: RuleDescriptor, line: str, rules_map: RulesMap, index: int = 0) -> int:
+    if rule.type == RuleType.SimpleRule:
+        return _match_simple_rule(rule, line, rules_map, index)
+    elif rule.type == RuleType.ChainedRule:
+        return _match_chained_rule(rule, line, rules_map, index)
+    elif rule.type == RuleType.FlexRule:
+        return _match_flex_rule(rule, line, rules_map, index)
 
-    def __str__(self):
-        return "(" + " | ".join([str(sr) for sr in self.sub_rules]) + ")"
+    raise ValueError(f"Invalid rule type, rule ID={rule.id}, rule type={rule.type}")
+
+
+def _match_simple_rule(rule: RuleDescriptor, line: str, _: RulesMap, index: int = 0) -> int:
+    return len(rule.data) if line.startswith(rule.data, index) else 0
+
+
+def _match_chained_rule(rule: RuleDescriptor, line: str, rules_map: RulesMap, index: int = 0) -> int:
+    sub_rules = rule.data
+    total_matched = 0
+    for sr in sub_rules:
+        sub_rule = rules_map[sr]
+        matched = _match_rule(sub_rule, line, rules_map, index + total_matched)
+        if matched == 0:
+            return 0
+        total_matched += matched
+
+    return total_matched
+
+
+def _match_flex_rule(rule: RuleDescriptor, line: str, rules_map: RulesMap, index: int = 0) -> Tuple[int]:
+    chained_rules_groups: Tuple[Tuple[int]] = rule.data
+    for crg in chained_rules_groups:
+        sub_rule = RuleDescriptor(RuleType.ChainedRule, crg)
+        matched = _match_rule(sub_rule, line, rules_map, index)
+        if matched > 0:
+            return matched
+
+    return 0
+
+
+def parse_rules(rules: Iterable[str]) -> RulesMap:
+    parsed_rules = [parse_rule(r) for r in rules]
+    return {rule_id: rule for rule_id, rule in parsed_rules}
+
+
+def parse_rule(rule: str) -> Tuple[int, RuleDescriptor]:
+    return _parse_simple_rule(rule) or _parse_compound_rule(rule)
 
 
 def _parse_simple_rule(rule: str):
-    SIMPLE_RULE_PATTERN = '(\d+): ("[a-z]+")'
+    SIMPLE_RULE_PATTERN = r'(\d+): ("[a-z]+")'
 
     m = regex.match(SIMPLE_RULE_PATTERN, rule)
     if not m:
         return None
-    
+
     groups = m.groups()
     rule_id = int(groups[0])
     rule_str = groups[1][1:-1]
-    return rule_id, SimpleRule, rule_str
+
+    # print(f"Rule #{rule_id}: Simple, {rule_str}")
+    return rule_id, RuleDescriptor(RuleType.SimpleRule, rule_str)
 
 
 def _parse_compound_rule(rule: str):
-    COMPOUND_RULE_PATTERN = '(\d+):( \d+)+( \|( \d+)+)?'
+    COMPOUND_RULE_PATTERN = r'(\d+):( \d+)+( \|( \d+)+)?'
 
     m = regex.match(COMPOUND_RULE_PATTERN, rule)
     if not m:
         return None
 
     groups = m.groups()
-    rule_type = ChainedRule
+    rule_type = RuleType.ChainedRule
     rule_id = int(groups[0])
-    sub_rules = (int(sr) for sr in m.captures(2))
+    sub_rules = tuple(int(sr) for sr in m.captures(2))
 
     if m.captures(4):
-        rule_type = FlexRule
-        sub_rules = (sub_rules, (int(sr) for sr in m.captures(4)))
+        rule_type = RuleType.FlexRule
+        sub_rules = (sub_rules, tuple(int(sr) for sr in m.captures(4)))
 
-    assert (('|' in rule and rule_type == FlexRule) or rule_type == ChainedRule)
-
-    return rule_id, rule_type, sub_rules
-
-
-def parse_rule(rule: str) -> Rule:
-    return _parse_simple_rule(rule) or _parse_compound_rule(rule)
-
-ParsedRulesMap = Dict[int, Tuple[Callable, Any]]
-
-
-def parse_rules(rules: Iterable[str]) -> ParsedRulesMap:
-    parsed_rules = [parse_rule(r) for r in rules]
-    return {rule_id: (rule_type, params) for rule_id, rule_type, params in parsed_rules}
-
-
-def build_rules(rules: ParsedRulesMap) -> Dict[int, Rule]:
-    built_rules = {}
-    for rid in rules:
-        _build_rule(rules, built_rules, rid)
-    return built_rules
-
-
-def _build_rule(rules, built_rules, rule_id):
-    if rule_id in built_rules:
-        return
-
-    rule_type, params = rules[rule_id]
-
-    if rule_type == SimpleRule:
-        built_rules[rule_id] = rule_type(params)
-    elif rule_type == ChainedRule:
-        sub_rules = []
-        for srid in params:
-            sub_rules.append(
-                built_rules[srid] if srid in built_rules else
-                _build_rule(rules, built_rules, srid)
-            )
-        built_rules[rule_id] = rule_type(tuple(sub_rules))
-    else:
-        flex_rule_sub_rules = []
-        for chained_group in params:
-            chained_rule_sub_rules = []
-            for srid in chained_group:
-                chained_rule_sub_rules.append(
-                    built_rules[srid] if srid in built_rules else
-                    _build_rule(rules, built_rules, srid)
-                )
-            chained_sub_rule = ChainedRule(tuple(chained_rule_sub_rules))
-            flex_rule_sub_rules.append(chained_sub_rule)
-
-        built_rules[rule_id] = rule_type(tuple(flex_rule_sub_rules))
-
-    return built_rules[rule_id]
+    # print(f"Rule #{rule_id}: {'Chained' if rule_type == RuleType.ChainedRule else 'Flex'}, {sub_rules}")
+    return rule_id, RuleDescriptor(rule_type, sub_rules)
 
 
 def main():
     lines = open("2020/19/input.txt", "r").readlines()
     rules_strings_separator = lines.index("\n")
     raw_rules = lines[:rules_strings_separator]
-    strings = [l[:-1] for l in lines[rules_strings_separator + 1:]]
+    strings = [line[:-1] for line in lines[rules_strings_separator + 1:]]
 
     print(f"# of rules: {len(raw_rules)}")
     print(f"# of strings: {len(strings)}")
 
-    parsed_rules = parse_rules(raw_rules)
-    rules = build_rules(parsed_rules)
+    rules_map = parse_rules(raw_rules)
 
     # Part 1:
-    lines_matching_rule_0 = filter(lambda s: rules[0].match(s, 0, True) != 0, strings)
-    print(f"Number of lines matching rule #0: {len(list(lines_matching_rule_0))}")
+    lines_matching_rule_0 = set(filter(lambda s: match_rule(0, s, rules_map) > 0, strings))
+    print(f"Number of lines matching rule #0: {len(lines_matching_rule_0)}")
+
+    # Part 2
 
 
 if __name__ == "__main__":
