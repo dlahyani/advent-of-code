af3a9af32a46817675fe316597592ec49875d69f
Daniel Lahyani <daniel.lahyani@gmail.com>
2022-12-07 22:16:00 +0200

    Improve AoC day 6 solution runtime complexity.

--- 2022/06/solution.py
+++ 2022/06/solution.py
@@ -1,18 +1,26 @@
 from collections import Counter
 
-
-def find_first_distinct_sequence(data: str, seq_len: int) -> int:
-    counter = Counter(data[0:seq_len])
-    distinct_seq_index = -1
-    for i in range(seq_len, len(data)):
-        if counter.most_common(1)[0][1] == 1:
-            distinct_seq_index = i
-            break
-        
-        counter.subtract({data[i-seq_len]: 1})
-        counter.update(data[i])
+def find_first_distinct_sequence(data: str, k: int) -> int:
+    # Scan data using a sliding window of size k and identify if the window contains distinct chars.
+    # Use a Counter to track the exact number of occurrences of all chars within the window. As we
+    # slide the window right we decrement the counter of left-most char in the window and increment
+    # the counter for the new char on the right.
+    # By removing items from the Counter when they reach count of 0 we promise that len(counter)
+    # is the number of unique chars in the window, thus being able to test uniqueness in O(1)
+    # complexity (assuming len(Counter) is an O(1) operation).
     
-    return distinct_seq_index
+    window_counter = Counter(data[0:k])
+    for i in range(k, len(data)):
+        if len(window_counter) == k:
+            return i
+        
+        w0, wk = data[i-k], data[i]
+        window_counter.subtract(w0)
+        if window_counter[w0] == 0:
+            del window_counter[w0]
+        window_counter.update(wk)
+    
+    return -1
     
 data = open("2022/06/input.txt").read()
 
