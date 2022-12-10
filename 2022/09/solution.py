73c947aa0b5300beb1cdeac1ba175e8983e87eec
Daniel Lahyani <daniel.lahyani@gmail.com>
2022-12-10 16:02:55 +0200

    Make solution functional and shorter.

--- 2022/09/solution.py
+++ 2022/09/solution.py
@@ -1,84 +1,44 @@
 
+from functools import reduce
 from typing import Iterable
 
 Position = tuple[int, int]
 Rope = list[Position]
 
-def simulate_rope_movement(rope_length: int, start_position: Position, instructions: Iterable[str]):
-    rope: Rope = [start_position] * rope_length
-    rope_movement: list[Rope] = [rope]
+def simulate_rope_movement(rope_length: int, start_position: Position, instructions: Iterable[str]) -> Iterable[Rope]:
+    return reduce(lambda rope_movement, i: rope_movement + move_rope(rope_movement[-1], i[0], i[1]),
+        map(lambda i: (i[0], int(i[1])),
+            map(lambda i: i.strip().split(), instructions)
+        ), [[start_position] * rope_length])
 
-    for i in instructions:
-        direction, steps = i.split(" ")
-        steps = int(steps)
-        rope_movement.extend(move_rope(rope_movement[-1], direction, steps))
-    
-    return rope_movement
-
-
-def move_rope(rope: Rope, direction: str, steps: int) -> list[Rope]:
-    rope_movement: list[Rope] = [rope]
-    for _ in range(steps):
-        rope_movement.append(move_rope_one_step(rope_movement[-1], direction))
-    return rope_movement[1:]
+def move_rope(rope: Rope, direction: str, steps: int) -> Iterable[Rope]:
+    return reduce(lambda m, _: m + [move_rope_one_step(m[-1], direction)], range(steps), [rope])[1:]
             
 def move_rope_one_step(rope: Rope, direction: str) -> Rope:
-    new_rope = [move_head(rope[0], direction)]
-    for i in range(1, len(rope)):
-        new_rope.append(rope_knot_follow(new_rope[i-1], rope[i]))
-    return new_rope
+    return reduce(lambda new_rope, i: new_rope + [rope_knot_follow(new_rope[i-1], rope[i])],
+        range(1, len(rope)), [move_rope_knot(rope[0], direction)])
 
-def move_head(head: Position, direction: str) -> Position:
-    if direction == "R":
-        head = head[0]+1, head[1]
-    elif direction == "U":
-        head = head[0], head[1]+1
-    elif direction == "L":
-        head = head[0]-1, head[1]
-    else:
-        head = head[0], head[1]-1
-    return head
+def move_rope_knot(knot: Position, direction: str) -> Position:
+    step = {"R": (1, 0), "U": (0, 1), "L": (-1, 0), "D": (0, -1)}[direction]
+    return (knot[0] + step[0], knot[1] + step[1])
+
+def sign(n: int) -> int:
+    return 0 if n == 0 else 1 if n > 0 else -1
 
 def rope_knot_follow(leading: Position, following: Position) -> Position:
     # Leading and following are adjacent, no need to move.
     if abs(following[0] - leading[0]) <= 1 and abs(following[1] - leading[1]) <= 1:
         return following
-    # Following is 2 steps to the right of leading, move following one step left.
-    elif following[0] - leading[0] > 1 and following[1] - leading[1] == 0:
-        following = following[0] - 1, following[1]
-    # Following is 2 steps to the right and above leading, move following one step left and one step down.
-    elif following[0] - leading[0] > 0 and following[1] - leading[1] > 0:
-        following = following[0] - 1, following[1] - 1
-    # Following is 2 steps to above leading, move following one step down.
-    elif following[0] == leading[0] and following[1] - leading[1] > 1:
-        following = following[0], following[1] - 1
-    # Following is 2 steps to the left and above leading, move following one step right and one step down.
-    elif following[0] - leading[0] < 0 and following[1] - leading[1] > 0:
-        following = following[0] + 1, following[1] - 1
-    # Following is 2 steps to the right of leading, move following one step right.
-    elif following[0] - leading[0] < -1 and following[1] - leading[1] == 0:
-        following = following[0] + 1, following[1]
-    # Following is 2 steps to the left and below leading, move following one step right and one step up.
-    elif following[0] - leading[0] < 0 and following[1] - leading[1] < 0:
-        following = following[0] + 1, following[1] + 1
-    # Following is 2 steps to below leading, move following one step up.
-    elif following[0] == leading[0] and following[1] - leading[1] < -1:
-        following = following[0], following[1] + 1
-    # Following is 2 steps to the right and below leading, move following one step left and one step up.
-    else:
-        following = following[0] - 1, following[1] + 1
     
-    assert abs(following[0] - leading[0]) <= 1 and abs(following[1] - leading[1]) <= 1
-    return following
+    step = sign(leading[0] - following[0]), sign(leading[1] - following[1])
+    return following[0] + step[0] , following[1] + step[1]
 
-instructions = tuple(map(lambda l: l.strip(), open("2022/09/input.txt")))
+instructions = open("2022/09/input.txt").readlines()
 
 ### Part 1
-rope_movement = simulate_rope_movement(2, (0,0), instructions)
-tail_visited = set(map(lambda rp: rp[-1], rope_movement))
-print(f"Tail visited in {len(tail_visited)} unique spots")
+tail_positions = set(map(lambda rp: rp[-1], simulate_rope_movement(2, (0,0), instructions)))
+print(f"Rope length: 2 knots, Unique tail positions: {len(tail_positions)} ")
 
 ### Part 2
-rope_movement = simulate_rope_movement(10, (0,0), instructions)
-tail_visited = set(map(lambda rp: rp[-1], rope_movement))
-print(f"Tail visited in {len(tail_visited)} unique spots")
+tail_positions = set(map(lambda rp: rp[-1], simulate_rope_movement(10, (0,0), instructions)))
+print(f"Rope length: 10 knots, Unique tail positions: {len(tail_positions)}")
